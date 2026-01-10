c28f847e66a431ffb7679e8239c45ca4c246562c
Dana Michelle Vargas de Brasdefer <brasdefer1597@hotmail.com>
2026-01-09 20:25:01 -0600

    Revert "Refactor project structure and implement Demo vs Full entry points"

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { BuyMeCoffee } from './BuyMeCoffee';
 
 export const Footer: React.FC = () => {
   return (
     <footer className="w-full py-8 mt-12 border-t border-gray-800 text-center">
-      <div className="mb-6">
-        <BuyMeCoffee />
-      </div>
       <p className="text-gray-500 text-xs font-mono">
         Chalamandra Magistral © 2025 | DecoX Architecture
       </p>
